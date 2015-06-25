762419196d9fb1570af7cf63c4314a9e473242d4
Jesse Chen <jesse.z.chen@gmail.com>
2015-06-25 17:12:03 -0400

    Start action refactoring

--- dance_model.js
+++ dance_model.js
@@ -20,7 +20,7 @@ var Hands4 = function(center, radius, dance) {
 		this.repeat = repeatFlag;
 		this.moves = [];
 		this.defaultMove = new Stand(0,75);
-		this.danceLength = 0;
+		this.duration = 0;
 	};
 
 	Dance.prototype.getX = function(count, pos) {
@@ -35,7 +35,7 @@ var Hands4 = function(center, radius, dance) {
 
 	Dance.prototype.currentMove = function(count) {
 		if(this.repeat) {
-			count %= this.danceLength;
+			count %= this.duration;
 		}
 		for(var i = 0; i < this.moves.length; i++) {
 			if(count > this.moves[i].duration) {
@@ -49,28 +49,60 @@ var Hands4 = function(center, radius, dance) {
 
 	Dance.prototype.addMove = function(move) {
 		this.moves.push(move);
-		this.danceLength += move.duration;
+		this.duration += move.duration;
 	};
 
 	var Move = function() {
-
-	};
-
-	Move.prototype.getCoords = function(count,pos) {
-		var theta = tau/8 + (tau/4 * pos);
-		return [Math.cos(theta) * 75, Math.sin(theta) * 75];
+		this.actions = [];
+		this.defaultMove = new Wait(0,75);
+		this.duration = 0;
 	};
 
 	Move.prototype.getX = function(count, pos) {
-		return this.getCoords(count, pos)[0];
+		var actionInfo = this.currentAction(count);
+		return actionInfo[0].getX(actionInfo[1], pos);
 	};
-
+	
 	Move.prototype.getY = function(count, pos) {
-		return this.getCoords(count, pos)[1];
+		var actionInfo = this.currentAction(count);
+		return actionInfo[0].getY(actionInfo[1], pos);
+	};
+	
+	Move.prototype.currentAction = function(count) {
+		for(var i = 0; i < this.actions.length; i++) {
+			if(count > this.actions[i].duration) {
+				count -= this.actions[i].duration;
+			} else {
+				return [this.actions[i], count];
+			}
+		}
+		return [this.defaultAction, 0];
+	};
+	
+	Move.prototype.addAction = function(action) {
+		this.actions.push(action);
+		this.duration += action.duration;
 	};
 
 	Move.prototype.posTransform = function(pos) {
 		return pos;
+	};
+
+	var Action = function() {
+
+		Action.prototype.getCoords = function(count,pos) {
+			var theta = tau/8 + (tau/4 * pos);
+			return [Math.cos(theta) * 75, Math.sin(theta) * 75];
+		};
+
+		Action.prototype.getX = function(count, pos) {
+			return this.getCoords(count, pos)[0];
+		};
+
+		Action.prototype.getY = function(count, pos) {
+			return this.getCoords(count, pos)[1];
+		};
+
 	};
 
 	var Circle = function(duration, direction, radius) {
@@ -78,14 +110,11 @@ var Hands4 = function(center, radius, dance) {
 		this.direction = direction;
 		this.speed = 1/8;
 		this.duration = duration;
+		this.addAction(new Rotate(duration, direction, this.speed, radius));
 	};
 
-	Circle.prototype.__proto__ = Move.prototype;
-
-	Circle.prototype.getCoords = function(count, pos) {
-		var theta = tau/8 + (tau/4 * pos) + this.direction * this.speed * count * tau;
-		return [Math.cos(theta) * this.radius, Math.sin(theta) * this.radius];
-	};
+	Circle.prototype = new Move();
+	Circle.constructor = Circle;
 
 	Circle.prototype.posTransform = function(pos) {
 		var posDelta = this.direction * this.duration * this.speed * 4;
@@ -94,27 +123,59 @@ var Hands4 = function(center, radius, dance) {
 		return pos;
 	};
 
+	var Rotate = function(duration, direction, speed, radius) {
+		this.radius = radius;
+		this.direction = direction;
+		this.speed = speed;
+		this.duration = duration;
+	};
+
+	Rotate.prototype.__proto__ = Action.prototype;
+	
+	Rotate.prototype.getCoords = function(count, pos) {
+		var theta = tau/8 + (tau/4 * pos) + this.direction * this.speed * count * tau;
+		return [Math.cos(theta) * this.radius, Math.sin(theta) * this.radius];
+	};
+
 	var Stand = function(duration, radius) {
+		this.duration = duration;
+		this.radius = radius;
+		this.addAction(new Wait(duration, radius));
+	};
+
+	Stand.prototype.__proto__ = Move.prototype;
+
+	var Wait = function(duration, radius) {
 		this.duration = duration;
 		this.radius = radius;
 	};
 
-	Stand.prototype.__proto__ = Move.prototype;
+	Wait.prototype.__proto__ = Action.prototype;
 
 	var Allemande = function(duration, direction, radius) {
 		this.duration = duration;
 		this.direction = direction;
 		this.radius = radius;
 		this.speed = 1/4;
+		this.addAction(new Spin(duration, direction, this.speed, radius));
 	};
 
 	Allemande.prototype.__proto__ = Move.prototype;
 
-	Allemande.prototype.getCoords = function(count, pos) {
+	var Spin = function(duration, direction, speed, radius) {
+		this.duration = duration;
+		this.direction = direction;
+		this.speed = speed;
+		this.radius = radius;
+	};
+
+	Spin.prototype.__proto__ = Action.prototype;
+
+	Spin.prototype.getCoords = function(count, pos) {
 		var initialPositionFor = this.__proto__.__proto__.getCoords;
-        var startingPosition = initialPositionFor(0, pos);
+		var startingPosition = initialPositionFor(0, pos);
 		var partnerStartingPosition = initialPositionFor(0, pos^1);
-        var cx = (startingPosition[0] + partnerStartingPosition[0])/2;
+		var cx = (startingPosition[0] + partnerStartingPosition[0])/2;
 		var cy = (startingPosition[1] + partnerStartingPosition[1])/2;
 		var polar = cartesianToPolar([cx,cy],startingPosition);
 		var theta_delta = tau * this.speed * count * this.direction;

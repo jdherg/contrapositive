ca541cb2af002798fe70f00269a3128834daa66f
Jesse Chen <jesse.z.chen@gmail.com>
2015-06-30 16:38:20 -0400

    Extract ContraSet into a separate file

--- dance.js
+++ dance.js
@@ -4,36 +4,6 @@ var Dancer = function(id) {
     this.id = id;
 };
 
-var ContraSet = function (dance, dancers) {
-    this.dance = dance;
-    this.dancers = dancers;
-    this.top = [];
-    this.bottom = [];
-    this.dancerPositions = {};
-};
-
-ContraSet.prototype.getX = function (count, dancer) {
-    var dancerInfo = this.getGroupAndPos(dancer.id);
-    return this.center(dancerInfo[0])[0] + this.dance.getX(count, dancerInfo[1]);
-};
-
-ContraSet.prototype.getY = function (count, dancer) {
-    var dancerInfo = this.getGroupAndPos(dancer.id);
-    return this.center(dancerInfo[0])[1] + this.dance.getY(count, dancerInfo[1]);
-};
-
-ContraSet.prototype.center = function (hands4) {
-    return [250, 250+(hands4*200)];
-};
-
-ContraSet.prototype.getGroupAndPos = function(dancer_id, count) {
-    return [Math.floor(dancer_id / 4), dancer_id % 4];
-};
-
-ContraSet.prototype.numberOfProgressions = function(count) {
-    Math.floor(count / this.dance.duration);
-};
-
 var Dance = function (repeatFlag) {
     this.repeat = repeatFlag;
     this.moves = [];
--- test/ContraSetSpec.js
+++ test/ContraSetSpec.js
@@ -23,16 +23,24 @@ describe("ContraSet", function() {
         expect(contraSet.getGroupAndPos(7, 0)).toEqual([1, 3]);
     });
 
-    it("progresses once", function() {
-        expect(contraSet.getGroupAndPos(2, 17)).toEqual([0, 0]);
-        expect(contraSet.getGroupAndPos(3, 17)).toEqual([0, 1]);
-        expect(contraSet.getGroupAndPos(4, 17)).toEqual([1, 3]);
-        expect(contraSet.getGroupAndPos(5, 17)).toEqual([1, 2]);
+    it("knows what group a dancer belongs to after two progressions", function () {
+        expect(contraSet.getGroupAndPos(0, 34)).toEqual([0, 2]);
+        expect(contraSet.getGroupAndPos(1, 34)).toEqual([0, 3]);
+        expect(contraSet.getGroupAndPos(2, 34)).toEqual([1, 2]);
+        expect(contraSet.getGroupAndPos(3, 34)).toEqual([1, 3]);
+        expect(contraSet.getGroupAndPos(4, 34)).toEqual([0, 0]);
+        expect(contraSet.getGroupAndPos(5, 34)).toEqual([0, 1]);
+        expect(contraSet.getGroupAndPos(6, 34)).toEqual([1, 0]);
+        expect(contraSet.getGroupAndPos(7, 34)).toEqual([1, 1]);
     });
 
     it("handles being out", function() {
         expect(contraSet.getGroupAndPos(0, 17)).toEqual(["top", 2]);
         expect(contraSet.getGroupAndPos(1, 17)).toEqual(["top", 3]);
+        expect(contraSet.getGroupAndPos(2, 17)).toEqual([0, 0]);
+        expect(contraSet.getGroupAndPos(3, 17)).toEqual([0, 1]);
+        expect(contraSet.getGroupAndPos(4, 17)).toEqual([1, 3]);
+        expect(contraSet.getGroupAndPos(5, 17)).toEqual([1, 2]);
         expect(contraSet.getGroupAndPos(6, 17)).toEqual(["bottom", 0]);
         expect(contraSet.getGroupAndPos(7, 17)).toEqual(["bottom", 1]);
     });

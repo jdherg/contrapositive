3389e57c3da38e70809484914fe201360358240e
Jesse Chen <jesse.z.chen@gmail.com>
2015-07-01 13:50:54 -0400

    Survival of the fittest

--- contra_set.js
+++ contra_set.js
@@ -23,109 +23,54 @@ ContraSet.prototype.center = function (hands4) {
     return [250, 250+(hands4*200)];
 };
 
-// ContraSet.prototype.getGroupAndPos = function(dancer_id, count) {
-//     var numberOfHands4s = Math.floor(this.dancers.length / 4);
-//     var steps = Math.ceil(this.numberOfProgressions(count) / 2);
-//     var group = Math.floor(dancer_id / 4);
-//     var pos = dancer_id % 4;
+ ContraSet.prototype.getGroupAndPos = function(dancer_id, count) {
+     var numberOfHands4s = Math.floor(this.dancers.length / 4);
+     var numberOfProgressions = this.numberOfProgressions(count);
+     numberOfProgressions = numberOfProgressions % this.dancers.length;
+     var steps = Math.ceil(numberOfProgressions / 2);
+     var group = Math.floor(dancer_id / 4);
+     var pos = dancer_id % 4;
 
-//     if (pos < 2) {
-//         group -= steps;
-//     } else {
-//         group += steps;
-//     }
+     if (pos < 2) {
+         group -= steps;
+     } else {
+         group += steps;
+     }
 
-//     // bounce at the top
-//     if (group < 0) {
-//         group = (-1) - group;
-//         pos += 2;
-//     }
+     // bounce at the top
+     if (group < 0) {
+         group = (-1) - group;
+         pos += 2;
+     }
 
-//     // bounce at the bottom
-//     if (group >= numberOfHands4s) {
-//         group = (numberOfHands4s + 1) - group;
-//         pos -= 2;
-//     }
+     // bounce at the bottom
+     if (group >= numberOfHands4s) {
+         group = (numberOfHands4s + 1) - group;
+         pos -= 2;
+     }
 
-//     if (this.numberOfProgressions(count) % 2 === 1) {
-//         if (pos > 1) {
-//             group -= 1;
-//         }
+     // bounce at the top
+     if (group < 0) {
+         group = (-1) - group;
+         pos += 2;
+     }
 
-//         if(group === -1) {
-//             group = "top";
-//         }
+     if (numberOfProgressions % 2 === 1) {
+         if (pos > 1) {
+             group -= 1;
+         }
 
-//         if(group === numberOfHands4s - 1) {
-//             group = "bottom";
-//         }
-//     }
-//     return [group, pos];
-// };
+         if(group === -1) {
+             group = "top";
+         }
 
-ContraSet.prototype.getGroupAndPos = function(dancer_id, count) {
-    if(dancer_id % 2 === 1) {
-        var partner = this.getGroupAndPos(dancer_id - 1, count);
-        return [partner[0],partner[1]+1];
-    }
-    if(dancer_id === 0) {
-        return this.path(this.numberOfProgressions(count));
-    }
-    if(dancer_id === 2) {
-        return this.getGroupAndPos(0, count + 2 * this.dance.duration);
-    }
-    var base = dancer_id % 4;
-    if(base === 0) {
-        return this.getGroupAndPos(base, count + (this.progressionCap() - 1 - Math.floor(dancer_id/4)) * this.dance.duration );
-    } else {
-        return this.getGroupAndPos(base, count + (1 + Math.floor(dancer_id/4)) * this.dance.duration);
-    }
-
-}
+         if(group === numberOfHands4s - 1) {
+             group = "bottom";
+         }
+     }
+     return [group, pos];
+ };
 
 ContraSet.prototype.numberOfProgressions = function(count) {
     return Math.floor(count / this.dance.duration);
 };
-
-ContraSet.prototype.progressionCap = function() {
-    return this.dancers.length;
-}
-
-ContraSet.prototype.path =  function(progression) {
-    return([this.path_group(progression),this.path_pos(progression)]);
-}
-
-ContraSet.prototype.path_group = function(path_num) {
-    var progs = this.progressionCap();
-    path_num %= progs;
-    path_num -= 1;
-    if(path_num < 0) {
-        path_num += progs;
-    }
-    if(path_num === 0) {
-        return "top";
-    } else if (path_num === progs / 2) {
-        return "bottom";
-    } else {
-        var dist = Math.abs(progs/2 - path_num);
-        if(dist === 1) {
-            return progs/4 - 1;
-        } else {
-            return Math.floor(((progs/2 - dist) - 1)/2);
-        }
-    }
-}
-
-ContraSet.prototype.path_pos = function(path_num) {
-    var progs = this.progressionCap();
-    path_num %= progs;
-    path_num -= 1;
-    if(path_num < 0) {
-        path_num += progs;
-    }
-    if(path_num < progs / 2) {
-        return 2;
-    } else {
-        return 0;
-    }
-}

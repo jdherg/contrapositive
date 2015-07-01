5774e8b74968a3d6dea2c025ba2b9dbe2c035738
John Hergenroeder <jdherg@gmail.com>
2015-07-01 02:51:12 -0400

    Add tests for remaining progressions in 8 dancer dance.

--- test/ContraSetSpec.js
+++ test/ContraSetSpec.js
@@ -66,4 +66,39 @@ describe("ContraSet", function() {
         expect(contraSet.getGroupAndPos(6, 16)).toEqual([0, 0]);
         expect(contraSet.getGroupAndPos(7, 16)).toEqual([0, 1]);
     });
+
+    it("knows what group a dancer belongs to after five progressions", function () {
+        expect(contraSet.getGroupAndPos(0, 20)).toEqual(["bottom", 0]);
+        expect(contraSet.getGroupAndPos(1, 20)).toEqual(["bottom", 1]);
+        expect(contraSet.getGroupAndPos(2, 20)).toEqual([0, 0]);
+        expect(contraSet.getGroupAndPos(3, 20)).toEqual([0, 1]);
+        expect(contraSet.getGroupAndPos(4, 20)).toEqual([0, 2]);
+        expect(contraSet.getGroupAndPos(5, 20)).toEqual([0, 3]);
+        expect(contraSet.getGroupAndPos(6, 20)).toEqual(["top", 2]);
+        expect(contraSet.getGroupAndPos(7, 20)).toEqual(["top", 3]);
+    });
+
+    it("knows what group a dancer belongs to after six progressions", function () {
+        expect(contraSet.getGroupAndPos(0, 24)).toEqual([1, 0]);
+        expect(contraSet.getGroupAndPos(1, 24)).toEqual([1, 1]);
+        expect(contraSet.getGroupAndPos(2, 24)).toEqual([0, 0]);
+        expect(contraSet.getGroupAndPos(3, 24)).toEqual([0, 1]);
+        expect(contraSet.getGroupAndPos(4, 24)).toEqual([1, 2]);
+        expect(contraSet.getGroupAndPos(5, 24)).toEqual([1, 3]);
+        expect(contraSet.getGroupAndPos(6, 24)).toEqual([0, 2]);
+        expect(contraSet.getGroupAndPos(7, 24)).toEqual([0, 3]);
+    });
+
+    it("knows what group a dancer belongs to after seven progressions", function () {
+        expect(contraSet.getGroupAndPos(0, 28)).toEqual([0, 0]);
+        expect(contraSet.getGroupAndPos(1, 28)).toEqual([0, 1]);
+        expect(contraSet.getGroupAndPos(2, 28)).toEqual(["top", 2]);
+        expect(contraSet.getGroupAndPos(3, 28)).toEqual(["top", 3]);
+        expect(contraSet.getGroupAndPos(4, 28)).toEqual(["bottom", 0]);
+        expect(contraSet.getGroupAndPos(5, 28)).toEqual(["bottom", 1]);
+        expect(contraSet.getGroupAndPos(6, 28)).toEqual([0, 2]);
+        expect(contraSet.getGroupAndPos(7, 28)).toEqual([0, 3]);
+    });
+
+
 });

30d71d7615e2bfce54007ba4bc82729a8266f5ba
John Hergenroeder <jdherg@gmail.com>
2015-07-01 13:59:05 -0400

    Handle arbitary number of bounces.

--- contra_set.js
+++ contra_set.js
@@ -23,53 +23,50 @@ ContraSet.prototype.center = function (hands4) {
     return [250, 250+(hands4*200)];
 };
 
- ContraSet.prototype.getGroupAndPos = function(dancer_id, count) {
-     var numberOfHands4s = Math.floor(this.dancers.length / 4);
-     var numberOfProgressions = this.numberOfProgressions(count);
-     numberOfProgressions = numberOfProgressions % this.dancers.length;
-     var steps = Math.ceil(numberOfProgressions / 2);
-     var group = Math.floor(dancer_id / 4);
-     var pos = dancer_id % 4;
+ContraSet.prototype.getGroupAndPos = function(dancer_id, count) {
+    var numberOfHands4s = Math.floor(this.dancers.length / 4);
+    var numberOfProgressions = this.numberOfProgressions(count);
+    numberOfProgressions = numberOfProgressions % this.dancers.length;
+    var steps = Math.ceil(numberOfProgressions / 2);
+    var group = Math.floor(dancer_id / 4);
+    var pos = dancer_id % 4;
 
-     if (pos < 2) {
-         group -= steps;
-     } else {
-         group += steps;
-     }
+    // handle progression movement
+    if(pos < 2) {
+        group -= steps;
+    } else {
+        group += steps;
+    }
 
-     // bounce at the top
-     if (group < 0) {
-         group = (-1) - group;
-         pos += 2;
-     }
+    // handle bounces for out of bounds progression
+    while(group < 0 || group >= numberOfHands4s){
+        if (group < 0) {
+            group = (-1) - group;
+            pos += 2;
+        }
 
-     // bounce at the bottom
-     if (group >= numberOfHands4s) {
-         group = (numberOfHands4s + 1) - group;
-         pos -= 2;
-     }
+        if (group >= numberOfHands4s) {
+            group = (numberOfHands4s + 1) - group;
+            pos -= 2;
+        }
+    }
 
-     // bounce at the top
-     if (group < 0) {
-         group = (-1) - group;
-         pos += 2;
-     }
+    // handle notation quirks
+    if (numberOfProgressions % 2 === 1) {
+        if (pos >= 2) {
+            group -= 1;
+        }
 
-     if (numberOfProgressions % 2 === 1) {
-         if (pos > 1) {
-             group -= 1;
-         }
+        if(group === -1) {
+            group = "top";
+        }
 
-         if(group === -1) {
-             group = "top";
-         }
-
-         if(group === numberOfHands4s - 1) {
-             group = "bottom";
-         }
-     }
-     return [group, pos];
- };
+        if(group === numberOfHands4s - 1) {
+            group = "bottom";
+        }
+    }
+    return [group, pos];
+};
 
 ContraSet.prototype.numberOfProgressions = function(count) {
     return Math.floor(count / this.dance.duration);
